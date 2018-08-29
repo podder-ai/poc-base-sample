1f63dec1582b5405cae3d13ca30724e74d5eb8e9
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2018-08-29 10:41:55 +0700

    Enable logger to receive arguments

--- framework/logger.py
+++ framework/logger.py
@@ -7,15 +7,25 @@ class Logger(object):
         self.start_time = time.time()
         logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-    def warning(self, msg):
-        logging.warning(self.add_time(msg))
+    def warning(self, msg, *args, **kwargs):
+        logging.warning(self.add_time(msg), *args, **kwargs)
 
-    def info(self, msg):
-        logging.info(self.add_time(msg))
+    def warn(self, msg, *args, **kwargs):
+        logging.warning(self.add_time(msg), *args, **kwargs)
 
-    def debug(self, msg):
-        logging.debug(self.add_time(msg))
+    def info(self, msg, *args, **kwargs):
+        logging.info(self.add_time(msg), *args, **kwargs)
+
+    def debug(self, msg, *args, **kwargs):
+        logging.debug(self.add_time(msg), *args, **kwargs)
+
+    def log(self, msg, *args, **kwargs):
+        logging.log(self.add_time(msg), *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        logging.error(self.add_time(msg), *args, **kwargs)
 
     def add_time(self, msg):
         time_spent = round((time.time() - self.start_time), 3)
-        return "[{}] {}".format(time_spent, msg)
+        msg_with_time = "[{}] {}".format(time_spent, msg)
+        return msg_with_time
