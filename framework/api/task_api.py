3be3f9adfc727674b89a505f966883d34ce2e337
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2018-11-16 08:21:27 +0700

    Convert data to pass to task.py

--- framework/api/task_api.py
+++ framework/api/task_api.py
@@ -1,3 +1,4 @@
+import pipeline_framework_pb2
 import pipeline_framework_pb2_grpc
 
 from app import Task
@@ -8,5 +9,19 @@ class TaskApi(pipeline_framework_pb2_grpc.TaskApiServicer):
 
     def execute(self, request, _context):
         context = Context()
-        res = Task(context).execute(request.results)
-        return request
+        inputs = self._convert_to_input_data(request)
+        outputs = Task(context).execute(inputs)
+        task_response = self._convert_to_task_response(outputs)
+        return task_response
+
+    def _convert_to_input_data(self, request):
+        inputs = []
+        for result in request.results:
+            inputs.append({'group_id': result.group_id, 'content': result.content})
+        return inputs
+
+    def _convert_to_task_response(self, outputs):
+        task_response = pipeline_framework_pb2.TaskResponse()
+        for output in outputs:
+            task_response.results.add(group_id=output['group_id'], content=output['content'])
+        return task_response
